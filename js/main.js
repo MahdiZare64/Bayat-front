90f743b063b181882f4a50cd6338cabe9918dbd1
mzare0930@gmail.com <mzare0930@gmail.com>
2022-01-24 18:01:48 +0330

    updated

--- js/main.js
+++ js/main.js
@@ -14,20 +14,21 @@ Bayat.slider.init = function () {
       autoplayHoverPause: true,
       dots: true,
       items: 3,
+      
     });
 
     $(".immigration__countries__slider").owlCarousel({
-        loop: true,
-        margin: 30,
-        smartSpeed: 700,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        rtl: true,
-        nav: false,
-        autoplayHoverPause: true,
-        dots: false,
-        items: 11,
-      });
+      loop: true,
+      margin: 30,
+      smartSpeed: 700,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      rtl: true,
+      nav: false,
+      autoplayHoverPause: true,
+      dots: false,
+      items: 11,
+    });
   } catch (e) {
     console.log(`Error on Bayat.slider.init - ${e}`);
   }
@@ -37,21 +38,22 @@ Bayat.sliderTab = Bayat.sliderTab || {};
 Bayat.sliderTab.init = function () {
   try {
     const tabs = document.querySelectorAll(".immigration__slider__tabs__btn");
-    const tabWrapper = document.querySelector(".immigration__slider__tabs")
 
     if (tabs.length) {
       tabs.forEach(function (item, index) {
         item.addEventListener("click", function (e) {
           e.preventDefault();
+          const tabWrapper = item.parentElement;
 
-          const activeItem = document.querySelector(
+          const activeItem = tabWrapper.querySelector(
             ".immigration__slider__tabs__btn.active"
           );
           activeItem.classList.remove("active");
           item.classList.add("active");
 
-          tabWrapper.classList = `immigration__slider__tabs active-${index + 1}`
-
+          tabWrapper.classList = `immigration__slider__tabs active-${
+            (index + 1) % 3
+          }`;
         });
       });
     }
